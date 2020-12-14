10ed9e57125688cfb824576cc42ff0c119b67a58
Danilo Rivera <danilo.caramelogit@gmail.com>
2020-12-14 09:03:00 -0300

    feat: finished second activity

---
+++ PrimeiraParte/11-12-2020/cep.js
@@ -0,0 +1,16 @@
+fetch = require("node-fetch");
+let cepBuscado;
+console.log("Buscando CEP");
+cepBuscado = buscarCep("13845373");
+console.log("CEP buscado");
+console.log("CEP encontrado ", cepBuscado);
+
+function buscarCep(parametro) {
+    let cep;
+    fetch(`https://viacep.com.br/ws/${parametro}/json/`)
+        .then(response => response.json)
+        .then(data => {cep = data.cep
+            console.log("CEP encontrado: ", cep)})
+        .catch(console.error);
+    return cep;
+}
