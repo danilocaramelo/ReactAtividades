1b6e9db8f79c9af05b57ab2895ce0e0f071f7cdb
Danilo Rivera <danilo.caramelogit@gmail.com>
2020-12-15 14:15:16 -0300

    feat: done until step 22

--- PrimeiraParte/CRUDNode/server.js
+++ PrimeiraParte/CRUDNode/server.js
@@ -1,11 +1,43 @@
-const express = require('express');
-
-const app = express();
-
-app.listen(3000, function() {
-    console.log('servidor rodando na porta 3000');
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
+ 
+const ObjectId = require('mongodb').ObjectID
+ 
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://danilo:caramelo@cluster0.fdeso.mongodb.net/cursoReact?retryWrites=true&w=majority";
+ 
+MongoClient.connect(uri, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('cursoReact') // coloque o nome do seu DB
+ 
+  app.listen(3000, () => {
+    console.log('Server running on port 3000')
+  })
 })
+ 
+app.use(bodyparser.urlencoded({ extended: true}))
+ 
+app.set('view engine', 'ejs')
+ 
+app.get('/', function(req, res){
+    // var cursor = db.collection('data').find();
+    res.render('index.ejs');
+});
 
-app.get('/', function(req, res) {
-    res.send('Olá Mundo');
+app.get('/show', (req, res) => {
+    db.collection('data').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('shows.ejs', { data: results })
+    })
 })
+ 
+app.post('/show', (req, res)=>{
+    //criar a coleção “data”, que irá armazenar nossos dados
+    db.collection('data').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+     
+        console.log('Salvo no Banco de Dados')
+        res.redirect('/show')
+      })
+});
