ee1c12ca1d15d786c6ce30f1f9523ab2b3b02e37
Danilo Caramelo <danilo.caramelogit@gmail.com>
2021-01-04 10:10:23 -0300

    feat: increase local storage save

--- PrimeiraParte/21-12-2020/relogio-functions/src/components/Cronometer.jsx
+++ PrimeiraParte/21-12-2020/relogio-functions/src/components/Cronometer.jsx
@@ -1,36 +1,37 @@
-import {useEffect, useState} from 'react';
-import List from './List';
+import { useEffect, useState } from "react";
+import List from "./List";
 
 function Cronometer() {
   const [time, setTime] = useState("00:00:00.000");
-  const [timeReference, setTimeReference] = useState({timeReference: null});
-  const [savePoint, setSavePoint] = useState([])
+  const [timeReference, setTimeReference] = useState({ timeReference: null });
+  const [savePoint, setSavePoint] = useState([]);
+  const [key, setKey] = useState(0);
   const [isTurnOn, setIsTurnOn] = useState(false);
 
   useEffect(() => {
-    if(isTurnOn) {
-        const interval = setInterval(() => {
-            updateTime();
-          },50);
-          return () => clearInterval(interval);
+    if (isTurnOn) {
+      const interval = setInterval(() => {
+        updateTime();
+      }, 50);
+      return () => clearInterval(interval);
     }
-  })
+  });
 
   function updateTime() {
-      if(timeReference.timeReference == null) {
-          setTimeReference({timeReference: Date.now()});
-      }
-      let newHour = Date.now();
-      let stopWatch = newHour - timeReference.timeReference;
-      let miliseconds = stopWatch % 1000;
-      stopWatch = Math.floor(stopWatch / 1000);
-      let seconds = stopWatch % 60;
-      stopWatch = Math.floor(stopWatch / 60);
-      let minutes = stopWatch % 60;
-      stopWatch = Math.floor(stopWatch / 60);
-      let hours = stopWatch % 60;
+    if (timeReference.timeReference == null) {
+      setTimeReference({ timeReference: Date.now() });
+    }
+    let newHour = Date.now();
+    let stopWatch = newHour - timeReference.timeReference;
+    let miliseconds = stopWatch % 1000;
+    stopWatch = Math.floor(stopWatch / 1000);
+    let seconds = stopWatch % 60;
+    stopWatch = Math.floor(stopWatch / 60);
+    let minutes = stopWatch % 60;
+    stopWatch = Math.floor(stopWatch / 60);
+    let hours = stopWatch % 60;
 
-      setTime(hours + ":" + minutes + ":" + seconds + "." + miliseconds);
+    setTime(hours + ":" + minutes + ":" + seconds + "." + miliseconds);
   }
 
   function startStopWatch() {
@@ -38,25 +39,37 @@ function Cronometer() {
   }
 
   function stopCronometer() {
-      setTime("00:00:00.000");
-      setTimeReference({timeReference: null});
-      setIsTurnOn(false);
-      setSavePoint([]);
+    setTime("00:00:00.000");
+    setTimeReference({ timeReference: null });
+    setIsTurnOn(false);
+    setSavePoint([]);
+    localStorage.clear();
+    setKey(0);
   }
 
   function savePointFunction() {
-      setSavePoint(savePoint.concat(time));
+    localStorage.setItem(key, time);
+    setKey(key + 1);
+    parseLocalStorageIntoList();
   }
 
-    return(
-        <>
-            <div>{time}</div>
-            <button onClick={startStopWatch}>Iniciar / pausar</button>
-            <button onClick={stopCronometer}>Parar</button>
-            <button onClick={savePointFunction}>Salvar</button>
-            <List list={savePoint}></List>
-        </>
-    );
+  function parseLocalStorageIntoList() {
+    let list = [];
+    for (let i = 0; i < localStorage.length; i++){
+        list.push(localStorage.getItem(i));
+    }
+    setSavePoint(list);
+  }
+
+  return (
+    <>
+      <div>{time}</div>
+      <button onClick={startStopWatch}>Iniciar / pausar</button>
+      <button onClick={stopCronometer}>Parar</button>
+      <button onClick={savePointFunction}>Salvar</button>
+      <List list={savePoint}></List>
+    </>
+  );
 }
 
 export default Cronometer;
